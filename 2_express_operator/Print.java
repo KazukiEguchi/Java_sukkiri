833086ae805452b1fdd05ff6803a3fc94bbdb8ef
KazukiEguchi <kazzeguchi@icloud.com>
2022-04-04 07:01:35 +0000

    make 2_exp_ope

---
+++ 2_express_operator/Print.java
@@ -0,0 +1,9 @@
+public class Print{
+	public static void main(String[] args){
+		String name = "江口";
+		String message;
+		message = name + " hello!";
+		System.out.print(message);
+		System.out.print("改行ある？");
+	}
+}
---
+++ 2_express_operator/Println.java
@@ -0,0 +1,9 @@
+public class Println{
+	public static void main(String[] args){
+		String name = "江口";
+		String message;
+		message = name + " hello!";
+		System.out.println(message);
+		System.out.println("改行ある？");
+	}
+}
