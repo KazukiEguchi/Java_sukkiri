86d70daeabab2d4c3b2dae909d1111cf7b519249
hazama_k <hazama_k@hazama2.localdomain>
2022-06-10 12:19:12 +0900

    complete Chapter2

---
+++ 2_express_operator/Q2_1.java
@@ -0,0 +1,9 @@
+public class Q2_1{
+    public static void main(String[] args){
+        int x = 5;
+        int y = 10;
+        //x+yを計算してから型変換する
+        String ans = "x+yは" + (x + y);
+        System.out.println(ans);
+    }
+}
