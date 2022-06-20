af7ba254ae69ff70173c6058fa95df05a4c34374
hazama_k <hazama_k@hazama2.localdomain>
2022-06-21 00:14:13 +0900

    create chapter5

---
+++ 5_method/Q5_1.java
@@ -0,0 +1,15 @@
+public class Q5_1{
+    public static void main(String[] args){
+        introduceOneself();
+    }
+
+    public static void introduceOneself(){
+        String name = "江口";
+        int age = 24;
+        double height = 172.0;
+        char eto = '虎';
+
+        System.out.println("私は年齢が" + age + "歳、身長が" + height +"cm、干支が" +
+        eto +"の" + name + "です。");
+    }
+}
